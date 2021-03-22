af57a0a596834038e245a7acbe24a57860842691
Simon Kim <p9falcon@gmail.com>
2021-03-21 23:43:12 -0400

    - initial commit.

---
+++ src/iOS/ScreenMirroringServer/ScreenMirroringServer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ScreenMirroringServer
+//
+//  Created by Sung Jin Kim on 3/21/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
